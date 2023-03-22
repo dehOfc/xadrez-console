2abf0543c9e700ea9efd5c86e4849fe32434f4b0
André <nascimentoandre448@gmail.com>
2023-03-22 06:07:35 -0300

    Começando a implementar o tabuleiro.

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
+using Tabuleiro;
 
-namespace xadrez_console
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace xadrez_console {
+    class Program {
+        static void Main(string[] args) {
+
+            
+
+
+            Console.ReadLine();
         }
     }
 }
